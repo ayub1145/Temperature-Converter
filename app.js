ad263ee3fff32924c193764b4d6f377bdf920e63
Md. Ayub Ali <76879092+ayub1145@users.noreply.github.com>
2022-08-24 10:05:19 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+const searchInput = document.querySelector("#sInput");
+const popup = document.querySelector(".inputPopup");
+const background= document.querySelector(".searchIcons");
+
+
+searchInput.addEventListener("click",(e)=>{
+    popup.classList.toggle("popup")
+    
+})
+
